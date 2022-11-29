d6c958aaee54a2e838600d6038bfd9541b676021
Hayato1311 <c0a2109550@edu.teu.ac.jp>
2022-11-29 14:36:31 +0900

    練習問題(4)

--- ex02/calc.py
+++ ex02/calc.py
@@ -5,6 +5,9 @@ root = tk.Tk() #ウィンドウを作成
 root.title("tk") #ウィンドウ名を設定
 root.geometry("300x500") #ウィンドウサイズを設定
 
+entry = tk.Entry(root, justify="right", width=10, font=("", 40)) #入力欄を描画
+entry.grid(row=0,column=0, columnspan=3)#3グリッドに跨って描画
+
 
 #ボタンを押されたときに表示するメッセージボックスの関数
 def button_click(event):
@@ -13,7 +16,7 @@ def button_click(event):
     tkm.showinfo(txt, f"{txt}のボタンがクリックされました")
 
 
-r, c = 0, 0
+r, c = 1, 0
 for num in range(9, -1, -1):
     #ボタンを描画する。(ウィンドウインスタンス、ボタンの文字列、フォントタイプ・サイズ、幅、高さ)
     num = tk.Button(root, text=str(num), font=("", 30), width=4, height=2)
@@ -22,8 +25,8 @@ for num in range(9, -1, -1):
     num.bind("<1>", button_click)
 
     #ボタンを配置する。grid(row=行数、column=列数)、左上が(0,0)で下(右)に行くにつれて増える
-    c += 1
     num.grid(row=r, column=c)
+    c += 1
     if c%3 == 0:
         r += 1
         c = 0
