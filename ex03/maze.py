73af2d6c7f6ce0aeb7f6f70dbff71f768affb6a9
Hayato1311 <c0a2109550@edu.teu.ac.jp>
2022-12-06 15:44:34 +0900

    main_procの可読性向上

--- ex03/maze.py
+++ ex03/maze.py
@@ -19,15 +19,14 @@ def key_up(event):
 #リアルタイム処理をするmain_proc関数
 def main_proc():
     global cx, cy, mx, my #こうかとんの座標,マス番号をグローバル宣言
-    if key == "Up"   : my -= 1 #上方向への移動
-    if key == "Down" : my += 1 #下方向への移動
-    if key == "Left" : mx -= 1 #左方向への移動
-    if key == "Right": mx += 1 #右方向への移動
-    if maze_lst[mx][my] == 1:  #移動先が壁であれば
-            if key == "Up"   : my += 1 #上方向への移動(戻す)
-            if key == "Down" : my -= 1 #下方向への移動(戻す)
-            if key == "Left" : mx += 1 #左方向への移動(戻す)
-            if key == "Right": mx -= 1 #右方向への移動(戻す)
+    if key == "Up"    and maze_lst[mx][my-1] != 1: #上キーが押され、移動先が壁でなければ
+            my -= 1 #上方向へ移動する
+    if key == "Down"  and maze_lst[mx][my+1] != 1: #下キーが押され、移動先が壁でなければ
+            my += 1 #下方向へ移動する
+    if key == "Left"  and maze_lst[mx-1][my] != 1: #左キーが押され、移動先が壁でなければ
+            mx -= 1 #左方向へ移動する
+    if key == "Right" and maze_lst[mx+1][my] != 1: #右キーが押され、移動先が壁でなければ
+            mx += 1 #右方向へ移動する
     cx, cy = mx*100+50, my*100+50 #マス単位で移動するように値を代入
     canvas.coords("kokaton", cx, cy) #tag"kokaton"を動かす
     root.after(100, main_proc) #100ミリ秒後にもう一度main_procを動かす
@@ -39,17 +38,13 @@ if __name__ == "__main__":
     root.title("迷えるこうかとん") #ウィンドウ名を設定
     canvas = tk.Canvas(root, width=1500, height=900, bg="black") #キャンバスを描画
     canvas.pack() #キャンバスをpackする
-
     maze_lst = mm.make_maze(15,9) #15(ヨコ)x9(タテ)の迷路(2次元リスト)を作成,1が壁,0が床
     mm.show_maze(canvas, maze_lst) #作成した迷路を描画する
-
     mx, my = 1, 1 #マス番号を初期化
     cx, cy = mx*100+50, my*100+50 #こうかとんのx座標,y座標
-    kokaton = tk.PhotoImage(file="fig/8.png") #こうかとんの画像ファイルを指定
-
+    kokaton = tk.PhotoImage(file="fig/2.png") #こうかとんの画像ファイルを指定
     key = "" #変数keyを空文字""で初期化
     canvas.create_image(cx, cy, image=kokaton, tag="kokaton") #キャンバス上にこうかとんを配置
-    
     root.bind("<KeyPress>", key_down) #keyが押されたらkey_down関数を呼び出す
     root.bind("<KeyRelease>", key_up) #keyが離されたらkey_up関数を呼び出す
     main_proc() #main_proc関数を呼び出す
