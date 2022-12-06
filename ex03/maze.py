06991a7f84fa723596db449806af57080d3a60d9
Hayato1311 <c0a2109550@edu.teu.ac.jp>
2022-12-06 14:13:27 +0900

    練習問題(1)

---
+++ ex03/maze.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+import tkinter.messagebox as tkm
+
+
+if __name__ == "__main__":
+    root = tk.Tk() #ウィンドウを作成
+    root.title("迷えるこうかとん") #ウィンドウ名を設定
+    root.mainloop() #ウィンドウを表示
