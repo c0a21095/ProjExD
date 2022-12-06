e6bb955d1684fa5be1073e8094d2ac0302a945a5
Hayato1311 <c0a2109550@edu.teu.ac.jp>
2022-12-06 14:47:09 +0900

    練習問題(5,6)

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,7 +1,9 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
+#必要なモジュールの呼び出し
 
 
+#メイン関数
 def main():
     root = tk.Tk() #ウィンドウを作成
     root.title("迷えるこうかとん") #ウィンドウ名を設定
@@ -10,8 +12,22 @@ def main():
     cx, cy = 300, 400 #こうかとんのx座標,y座標
     kokaton = tk.PhotoImage(file="fig/8.png") #こうかとんの画像ファイルを指定
     key = "" #変数keyを空文字""で初期化
+    root.bind("<KeyPress>", key_down) #keyが押されたらkey_down関数を呼び出す
+    root.bind("<KeyRelease>", key_up) #keyが離されたらkey_up関数を呼び出す
     canvas.create_image(cx, cy, image=kokaton, tag="kokaton") #キャンバス上にこうかとんを配置
     root.mainloop() #ウィンドウを表示
+
+
+#key_down関数
+def key_down(event):
+    global key #keyをグローバル宣言
+    key = event.keysym #押されたキーのシンボルをkeyに代入する
+
+
+#key_up関数
+def key_up(event):
+    global key #keyをグローバル宣言
+    key = "" #keyに空文字を代入,どのキーも押されていないことにする
 
 
 #コマンドラインから実行されたら
