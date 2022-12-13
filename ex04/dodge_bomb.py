7d0141190ef16484472e9f95087cdfcd2e4784a1
Hayato1311 <c0a2109550@edu.teu.ac.jp>
2022-12-13 15:30:36 +0900

    3限基本機能実装完了

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -29,6 +29,7 @@ def main():
     bomb_rct.centerx = random.randint(0, scrn_rct.width) #x座標をランダムに
     bomb_rct.centery = random.randint(0, scrn_rct.height) #y座標をランダムに
     scrn_sfc.blit(bomb_sfc, bomb_rct) #爆弾を貼り付け
+    vx, vy = 1, 1 #移動速度を設定
 
 
     #Trueになるまで繰り返す
@@ -41,19 +42,48 @@ def main():
                 return #main関数からreturnする
         
         key_dict = pg.key.get_pressed() #すべてのキーの入力状態に関する辞書を返す
-        if key_dict[pg.K_UP]   : tori_rct.centery -= 1 #上方向に移動
-        if key_dict[pg.K_DOWN] : tori_rct.centery += 1 #下方向に移動
-        if key_dict[pg.K_LEFT] : tori_rct.centerx -= 1 #左方向に移動
-        if key_dict[pg.K_RIGHT]: tori_rct.centerx += 1 #右方向に移動
-
-        vx, vy = +1, 1 #移動速度を設定
-        bomb_rct.move_ip(vx, vy) #爆弾をvx,vyぶん移動させる
-
+        if key_dict[pg.K_UP]: 
+            tori_rct.centery -= 1 #上方向に移動
+        if key_dict[pg.K_DOWN]: 
+            tori_rct.centery += 1 #下方向に移動
+        if key_dict[pg.K_LEFT]: 
+            tori_rct.centerx -= 1 #左方向に移動
+        if key_dict[pg.K_RIGHT]: 
+            tori_rct.centerx += 1 #右方向に移動
+        if check_bound(tori_rct, scrn_rct) != (1, 1):
+            # どこかしらはみ出ていたら
+            if key_dict[pg.K_UP]:
+                tori_rct.centery += 1
+            if key_dict[pg.K_DOWN]:
+                tori_rct.centery -= 1
+            if key_dict[pg.K_LEFT]:
+                tori_rct.centerx += 1
+            if key_dict[pg.K_RIGHT]:
+                tori_rct.centerx -= 1 
         scrn_sfc.blit(tori_sfc, tori_rct) #scrn_sfcにtori_sfcを貼り付ける。tori_rctに従って
+
+        bomb_rct.move_ip(vx, vy) #爆弾をvx,vyぶん移動させる
         scrn_sfc.blit(bomb_sfc, bomb_rct) #爆弾を貼り付け
+        yoko, tate = check_bound(bomb_rct, scrn_rct) #反射の関数を呼び出し
+        vx *= yoko
+        vy *= tate
+
+        if tori_rct.colliderect(bomb_rct):
+            return
+
         pg.display.update() #ディスプレイ全体を更新。updateしないと表示されない
         clock.tick(1000) #1秒あたり1000フレームの時を刻む(whileに制限をかける)
 
+
+#反射するか否かを決める関数
+def check_bound(obj_rct, scr_rct):
+    #範囲内なら+1,範囲外なら-1
+    yoko, tate = 1, 1 #タテとヨコを初期化
+    if obj_rct.left < scr_rct.left or scr_rct.right < obj_rct.right: #左右が範囲外になったら
+        yoko = -1 #逆方向に
+    if obj_rct.top < scr_rct.top or scr_rct.bottom < obj_rct.bottom: #上下が範囲外になったら
+        tate = -1 #逆方向に
+    return yoko, tate #タテとヨコを返す
 
 if __name__ == "__main__":
     pg.init() #pygameモジュールを初期化(毎回やる)
