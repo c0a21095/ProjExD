98b4767e821c2cf6f62e818205269e8d35114650
Hayato1311 <c0a2109550@edu.teu.ac.jp>
2022-12-13 14:16:55 +0900

    練習問題(2)

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -5,19 +5,22 @@ import sys
 
 #ゲームの処理を書く
 def main():
-    clock = pg.time.Clock() #時間計測用オブジェクト
+    clock = pg.time.Clock() #時間計測用のオブジェクト
 
     pg.display.set_caption("逃げろ！こうかとん") #ウィンドウ名
     scrn_sfc = pg.display.set_mode((1600, 900)) #ウィンドウサイズ
-
     pg_bg_sfc = pg.image.load("fig/pg_bg.jpg") #背景画像を指定
-
     pg_bg_rct = pg_bg_sfc.get_rect() #rectを取り出す
-    scrn_sfc.blit(pg_bg_sfc, pg_bg_rct) #rectに従い背景を設定
 
-    pg.display.update() #ディスプレイ全体を更新。updateしないと表示されない
-
-    clock.tick(1) #1秒あたり1フレームの時を刻む
+    #Trueになるまで繰り返す
+    while True:
+        scrn_sfc.blit(pg_bg_sfc, pg_bg_rct) #rectに従い背景を設定
+        for event in pg.event.get():
+            #×ボタンが押されたら
+            if event.type == pg.QUIT:
+                return #main関数からreturnする
+        pg.display.update() #ディスプレイ全体を更新。updateしないと表示されない
+        clock.tick(1000) #1秒あたり1000フレームの時を刻む(whileに制限をかける)
 
 
 if __name__ == "__main__":
